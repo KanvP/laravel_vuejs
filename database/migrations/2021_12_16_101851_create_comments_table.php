dab2880ea7a73299793eb8b9dddcb2b43ccfdf15
KanvP <kanvp@outlook.com>
2021-12-20 09:05:34 +0700

    add migrate

--- database/migrations/2021_12_16_101851_create_comments_table.php
+++ database/migrations/2021_12_16_101851_create_comments_table.php
@@ -15,6 +15,10 @@ class CreateCommentsTable extends Migration
     {
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
+            $table->text('body');
+            $table->integer('reply_id');
+            $table->integer('blog_id');
+            $table->integer('user_id');
             $table->timestamps();
         });
     }
